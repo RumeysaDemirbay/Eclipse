b8f9a9f88ef88267bc64872da143b9df252f3afe
RumeysaDemirbay <114082197+RumeysaDemirbay@users.noreply.github.com>
2022-10-29 23:45:28 +0300

    oop1

---
+++ oop1/src/oop1/Product.java
@@ -0,0 +1,50 @@
+package oop1;
+
+public class Product {
+	private String name;
+	private double unitPrice;
+	private double discount;
+	private String imageUrl;
+	private int unitsİnstock;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public int getUnitsİnstock() {
+		return unitsİnstock;
+	}
+
+	public void setUnitsİnstock(int unitsİnstock) {
+		this.unitsİnstock = unitsİnstock;
+	}
+
+}
