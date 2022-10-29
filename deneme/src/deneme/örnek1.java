d3d52c937df382d6474c6d6778da2aa7beae27f6
RumeysaDemirbay <114082197+RumeysaDemirbay@users.noreply.github.com>
2022-10-29 23:39:42 +0300

    Deneme1

---
+++ deneme/src/deneme/örnek1.java
@@ -0,0 +1,22 @@
+package deneme;
+
+public class örnek1 {
+
+	public static void main(String[] args) {
+		
+		int sayi1 =32;
+		int sayi2 =67;
+		int sayi3 =45;
+		int enBuyuk =sayi1;
+		
+		if(enBuyuk<sayi2) {
+			enBuyuk=sayi2;
+		}
+		if(enBuyuk<sayi3) {
+			enBuyuk=sayi3;
+		}
+		
+		System.out.println("En buyuk= " +enBuyuk);
+	}
+
+}
